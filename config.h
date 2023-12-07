85a73bb97d128dbac08ab00c47b8de65bbf9a82a
Jason Pena <jasonpena@awkless.com>
2023-12-07 07:16:31 -0700

    feat: Apply center patch
    https://tools.suckless.org/dmenu/patches/center/
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- config.h
+++ config.h
@@ -1,22 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom */
+static int centered = 1;    /* -c option; centers dmenu on screen */
+static int min_width = 500; /* minimum width when centered */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Hurmit Nerd Font:style=Regular:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#deb88d", "#09141b" },
+	[SchemeSel] = { "#09141b", "#fca02f" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
